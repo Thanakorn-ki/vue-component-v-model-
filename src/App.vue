016bcb485414e36032eb887cfcb6c1d4e2038f1e
salapao2136 <thanakornaum11@gmail.com>
2017-04-02 21:55:12 +0700

    v2

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <text-box title="Thanakorns" v-model="firstname"></text-box>
+    {{firstname}}
+  </div>
+</template>
+
+<script>
+import TextBox from './TextBox.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      firstname: ''
+    }
+  },
+  components: {
+    TextBox
+  }
+}
+</script>
